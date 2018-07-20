15279be1211c8a3c3106634bde6d152f7588db5d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-20 15:35:14 -0400

    Fox Run Square 1.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -85,7 +85,7 @@
                 this.loadData().then(response => {
                     var socialFeed = response[0].data;
                     var social_feed = socialFeed.social.instagram;
-                    this.instaFeed = _.slice(social_feed, [0], [14]);
+                    this.instaFeed = _.slice(social_feed, [0], [7]);
                     this.instaFeed.map(insta => {
                         if(insta.caption != null){
                             insta.caption.text = _.truncate(insta.caption.text, { 'length': 60, 'separator': ' ' });
