bed89a113eef6fff4a4f81c7f3e1803870949488
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-24 17:21:31 -0400

    Fox Run Square 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -11,109 +11,89 @@
                     </div>
                 </div>
                 <div class="main_container margin_30">
-                    <!--<div class="hidden_phone">-->
-                        <div class="row store_nav">
-                            <div class="col-md-3">
-                                <a class="store_nav_link active_store_nav" href="/stores">Directory</a>
-                            </div>
-                            <div class="col-md-3">
-                                <a class="store_nav_link" href="/map">Center Map</a>
-                            </div>
-                            <div class="col-md-3">
-                                <span>Sort By: </span>
-                                <a class="store_nav_link" v-on:click="changeMode('alphabetical')">Alphabetical</a>
-                            </div>
-                            <div class="col-md-3">
-                                <v-select v-model="selectedCat" :options="dropDownCats" :searchable="false" :on-change="filteredByCategory" class="category-select" :placeholder="$t('stores_page.sort_by_cats')" id="selectByCat"></v-select>
-                            </div>
+                    <div class="row store_nav">
+                        <div class="col-md-3">
+                            <a class="store_nav_link active_store_nav" href="/stores">Directory</a>
                         </div>
-                        <div class="row center">
-                            <div class="col-md-12">
-                                <span class="legend"><span class="promo_exist"><i class="fas fa-tag"></i></span> Promotion</span>  
-                                <span class="legend"><span class="new_store"><i class="fas fa-star"></i></span> New Store </span>
-                                <span class="legend"><span class="coming_soon_store"><i class="far fa-clock"></i></span> Coming Soon Store</span>
-                            </div>
+                        <div class="col-md-3">
+                            <a class="store_nav_link" href="/map">Center Map</a>
                         </div>
-                        <div class="row" v-if="sortByStores">
-                            <div class="col-md-6">
-                                <div v-if="listOne" v-for="(stores, index) in listOne">
-                                    <div class="list_header">
-                                        <div class="store_initial_container">
-                                            {{index}}
-                                        </div>
-                                    </div>
-                                    <div class="store-section" v-for="store in stores">
-                                        <p class="store_list_name">
-                                            <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
-                                                {{store.name}}
-                                            </router-link>
-                                            <span v-if="store.is_new_store" class="pull-right new_store"><i class="fas fa-star"></i></span>
-                                            <span v-if="store.is_coming_soon_store" class="pull-right coming_soon_store"><i class="far fa-clock"></i></span>
-                                            <span v-if="store.promotions != null" class="promo_exist pull-right"><i class="fas fa-tag"></i></span>
-                                        </p>
+                        <div class="col-md-3">
+                            <span>Sort By: </span>
+                            <a class="store_nav_link" v-on:click="changeMode('alphabetical')">Alphabetical</a>
+                        </div>
+                        <div class="col-md-3">
+                            <v-select v-model="selectedCat" :options="dropDownCats" :searchable="false" :on-change="filteredByCategory" class="category-select" :placeholder="$t('stores_page.sort_by_cats')" id="selectByCat"></v-select>
+                        </div>
+                    </div>
+                    <div class="row center">
+                        <div class="col-md-12">
+                            <span class="legend"><span class="promo_exist"><i class="fas fa-tag"></i></span> Promotion</span>  
+                            <span class="legend"><span class="new_store"><i class="fas fa-star"></i></span> New Store </span>
+                            <span class="legend"><span class="coming_soon_store"><i class="far fa-clock"></i></span> Coming Soon Store</span>
+                        </div>
+                    </div>
+                    <div class="row" v-if="sortByStores">
+                        <div class="col-md-6">
+                            <div v-if="listOne" v-for="(stores, index) in listOne">
+                                <div class="list_header">
+                                    <div class="store_initial_container">
+                                        {{index}}
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6" >
-                                <div v-if="listTwo" v-for="(stores, index) in listTwo">
-                                    <div class="list_header">
-                                        <div class="store_initial_container">
-                                            {{index}}
-                                        </div>
-                                    </div>
-                                    <div class="store-section" v-for="store in stores">
-                                        <p class="store_list_name">
-                                            <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
-                                                {{store.name}}
-                                            </router-link>
-                                            <span v-if="store.is_new_store" class="pull-right new_store"><i class="fas fa-star"></i></span>
-                                            <span v-if="store.is_coming_soon_store" class="pull-right coming_soon_store"><i class="far fa-clock"></i></span>
-                                            <span v-if="store.promotions != null" class="promo_exist pull-right"><i class="fas fa-tag"></i></span>
-                                        </p>
-                                    </div>
+                                <div class="store-section" v-for="store in stores">
+                                    <p class="store_list_name">
+                                        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
+                                            {{store.name}}
+                                        </router-link>
+                                        <span v-if="store.is_new_store" class="pull-right new_store"><i class="fas fa-star"></i></span>
+                                        <span v-if="store.is_coming_soon_store" class="pull-right coming_soon_store"><i class="far fa-clock"></i></span>
+                                        <span v-if="store.promotions != null" class="promo_exist pull-right"><i class="fas fa-tag"></i></span>
+                                    </p>
                                 </div>
                             </div>
                         </div>
-                        <div class="row" v-else>
-                            <div class="col-md-12">
-                                <div v-for="(stores, index) in filteredStores" >
-                                    <div class="list_header">
-                                        <div class="store_initial_container">
-                                            {{index}}
-                                        </div>
+                        <div class="col-md-6" >
+                            <div v-if="listTwo" v-for="(stores, index) in listTwo">
+                                <div class="list_header">
+                                    <div class="store_initial_container">
+                                        {{index}}
                                     </div>
-                                    <div class="store-section" v-for="store in stores">
-                                        <p class="store_list_name">
-                                            <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
-                                                {{store.name}}
-                                            </router-link>
-                                            <span v-if="store.is_new_store" class="pull-right new_store"><i class="fas fa-star"></i></span>
-                                            <span v-if="store.is_coming_soon_store" class="pull-right coming_soon_store"><i class="far fa-clock"></i></span>
-                                            <span v-if="store.promotions != null" class="promo_exist pull-right"><i class="fas fa-tag"></i></span>
-                                        </p>
-                                    </div>
+                                </div>
+                                <div class="store-section" v-for="store in stores">
+                                    <p class="store_list_name">
+                                        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
+                                            {{store.name}}
+                                        </router-link>
+                                        <span v-if="store.is_new_store" class="pull-right new_store"><i class="fas fa-star"></i></span>
+                                        <span v-if="store.is_coming_soon_store" class="pull-right coming_soon_store"><i class="far fa-clock"></i></span>
+                                        <span v-if="store.promotions != null" class="promo_exist pull-right"><i class="fas fa-tag"></i></span>
+                                    </p>
                                 </div>
                             </div>
                         </div>
-                    <!--</div>-->
-                    <!--<div class="visible_phone">-->
-                    <!--    <div class="row margin_40">-->
-                    <!--        <div class="col-md-12 mobile_store_select">-->
-                    <!--            <v-select -->
-                    <!--                :options="storesList" -->
-                    <!--                :placeholder="'Select A Store'" -->
-                    <!--                :searchable="false" -->
-                    <!--                :label="'name'" -->
-                    <!--                :on-change="dropPin"-->
-                    <!--            ></v-select> -->
-                    <!--        </div>-->
-                    <!--    </div>-->
-                    <!--    <div class="row">-->
-                    <!--        <div class="col-md-12">-->
-                    <!--            <mapplic-png-map ref="pngmap_ref" :height="700" :hovertip="true" :storelist="storesList" :floorlist="floorList" :bindLocationOpened="true" :svgWidth="property.map_image_width" :svgHeight="property.map_image_height" :showPin="true" tooltiplabel="View Store Details"></mapplic-png-map>-->
-                    <!--        </div>-->
-                    <!--    </div>    -->
-                    <!--</div>-->
+                    </div>
+                    <div class="row" v-else>
+                        <div class="col-md-12">
+                            <div v-for="(stores, index) in filteredStores" >
+                                <div class="list_header">
+                                    <div class="store_initial_container">
+                                        {{index}}
+                                    </div>
+                                </div>
+                                <div class="store-section" v-for="store in stores">
+                                    <p class="store_list_name">
+                                        <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">
+                                            {{store.name}}
+                                        </router-link>
+                                        <span v-if="store.is_new_store" class="pull-right new_store"><i class="fas fa-star"></i></span>
+                                        <span v-if="store.is_coming_soon_store" class="pull-right coming_soon_store"><i class="far fa-clock"></i></span>
+                                        <span v-if="store.promotions != null" class="promo_exist pull-right"><i class="fas fa-tag"></i></span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </transition>
@@ -121,7 +101,7 @@
 </template>
 
 <script>
-    define(["Vue", "vuex", "vue-select", "vue!mapplic-png-map"], function(Vue, Vuex, VueSelect, MapplicComponent) {
+    define(["Vue", "vuex", "vue-select"], function(Vue, Vuex, VueSelect) {
         Vue.component('v-select', VueSelect.VueSelect);
         return Vue.component("stores-component", {
             template: template, // the variable template will be injected
@@ -187,31 +167,6 @@
                     var cats = _.map(this.processedCategories, 'name');
                     cats.unshift('All');
                     return cats;
-                },
-                storesList() {
-                    this.processedStores.map(function(store){
-                        store.zoom = 1;
-                    })
-                    return this.processedStores;
-                },
-                getPNGurl() {
-                    return "https://www.mallmaverick.com" + this.property.map_url;
-                },
-                pngMapRef() {
-                    return this.$refs.pngmap_ref;
-                },
-                floorList () {
-                    var floor_list = [];
-                    
-                    var floor_1 = {};
-                    floor_1.id = "first-floor";
-                    floor_1.title = "Floor 1";
-                    floor_1.map = this.getPNGurl;
-                    floor_1.z_index = 1;
-                    floor_1.show = true;
-                    
-                    floor_list.push(floor_1);
-                    return floor_list;
                 }
             },
             methods: {
@@ -249,9 +204,6 @@
                         sortedCats = _.groupBy(filtered, store => store.currentCategory);
                         this.filteredStores = sortedCats;
                     }
-                },
-                dropPin(store) {
-                    this.pngMapRef.showLocation(store.id);
                 }
             }
         });
