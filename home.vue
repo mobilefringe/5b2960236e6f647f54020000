df93b032f5395a2c28a26c0507756dbd5fb02687
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-18 18:02:32 -0500

    Fox Run Square 1.1  /home.vue

--- home.vue
+++ home.vue
@@ -67,7 +67,8 @@
                 ...Vuex.mapGetters([
                     'property',
                     'timezone',
-                    'getPropertyHours'
+                    'getPropertyHours',
+                    'findMetaDataByPath'
                 ]),
                 homeBanners() {
                     var banners = [];
